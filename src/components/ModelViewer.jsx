f261a914e05c14e7d5104ec2bf607ca0a2bc693e
Kaival <87368466+Kaival1212@users.noreply.github.com>
2024-06-19 10:40:08 +0530

    inti

--- src/components/ModelViewer.jsx
+++ src/components/ModelViewer.jsx
@@ -1,20 +1,20 @@
-'use client';
-
 import { Canvas, useLoader } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import { Suspense } from 'react';
 
 export const ModelViewer = () => {
-    const myModel = useGLTF('/reactor.gltf');
+    const myModel = useGLTF('/reactor2.gltf');
 
     return (
         <Canvas style={{ height: '500px', width: '100%' }} camera={{ position: [0, 0, 5] }}>
             <Suspense fallback={null}>
-                <pointLight position={[-10, -10, -10]} color="#48cc90" intensity={500} />
-                <pointLight position={[10, 10, 10]} color="#36e2e2" intensity={500} />
+                <pointLight position={[-10, -10, -10]} intensity={500} />
+                <pointLight position={[10, 10, 10]} intensity={500} />
                 <primitive object={myModel.scene} scale={[1.5, 1.5, 1.5]} />
                 <OrbitControls enableZoom={true} />
             </Suspense>
         </Canvas>
     );
 };
+
+
